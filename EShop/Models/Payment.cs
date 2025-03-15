18161e220d4a90da3a6557baf71ece20245e4cee
Łukasz Gałek <lukaszgalek2004@gmail.com>
2025-03-15 20:21:28 +0100

    models

--- EShop/Models/Payment.cs
+++ EShop/Models/Payment.cs
@@ -2,5 +2,8 @@ namespace EShop.Models;
 
 public class Payment
 {
-    
+    DateTime Date { get; set; }= DateTime.Now;
+    public int amount { get; set; }
+    public Client client { get; set; }
+    public Order order { get; set; }
 }
